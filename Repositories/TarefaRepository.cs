8afe19aec809ab98f63b70e5859e34b07793dac8
Robert Moraes <robertmoraes13r@gmail.com>
2025-07-09 22:44:34 -0300

    feat: Cria TarefaRepository

---
+++ Repositories/TarefaRepository.cs
@@ -0,0 +1,16 @@
+﻿using ApiDeTarefas.Entities;
+
+namespace ApiDeTarefas.Repositories
+{
+    public class TarefaRepository : ITarefaRepository
+    {
+        private static readonly List<Tarefa> _tarefas = new List<Tarefa>();
+
+        private static int _nextId = 1;
+
+        public void Adicionar(Tarefa tarefa)
+        {
+            tarefa.Id = _nextId++;
+        }
+    }
+}
