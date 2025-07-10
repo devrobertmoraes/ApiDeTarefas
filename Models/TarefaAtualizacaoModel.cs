f5810ba0fd57ef6d80c2c73829033e9da4b2072f
Robert Moraes <robertmoraes13r@gmail.com>
2025-07-09 23:38:36 -0300

    Cria Models de atualizacao e Criacao

---
+++ Models/TarefaAtualizacaoModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiDeTarefas.Models
+{
+    public class TarefaAtualizacaoModel
+    {
+        [Required(ErrorMessage = "O título é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O título deve ter no máximo 100 caracteres.")]
+        public string Titulo {  get; set; }
+        public bool Concluida { get; set; }
+    }
+}
---
+++ Models/TarefaCriacaoModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiDeTarefas.Models
+{
+    public class TarefaCriacaoModel
+    {
+        [Required(ErrorMessage = "O título é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O título deve ter no máximo 100 caracteres.")]
+        public string Titulo { get; set; }
+    }
+}
