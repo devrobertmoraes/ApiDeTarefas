f2d4de216e9770a07e271edae5fc0225264f75b8
Robert Moraes <robertmoraes13r@gmail.com>
2025-07-09 23:24:31 -0300

    Cria ITarefaService e cria TarefaService

---
+++ Services/ITarefaService.cs
@@ -0,0 +1,13 @@
+﻿using ApiDeTarefas.Entities;
+
+namespace ApiDeTarefas.Services
+{
+    public interface ITarefaService
+    {
+        Tarefa CriarTarefa(string titulo);
+        List<Tarefa> ObterTodas();
+        Tarefa ObterPorId(int id);
+        void MarcarComoConcluida(int id, bool concluida);
+        bool RemoverTarefa(int id);
+    }
+}
---
+++ Services/TarefaService.cs
@@ -0,0 +1,67 @@
+﻿using ApiDeTarefas.Entities;
+using ApiDeTarefas.Repositories;
+
+namespace ApiDeTarefas.Services
+{
+    public class TarefaService : ITarefaService
+    {
+        private readonly ITarefaRepository _tarefaRepository;
+
+        public TarefaService(ITarefaRepository tarefaRepository)
+        {
+            _tarefaRepository = tarefaRepository;
+        }
+
+        public Tarefa CriarTarefa(string titulo)
+        {
+            if (string.IsNullOrWhiteSpace(titulo))
+            {
+                return null;
+            }
+
+            Tarefa novaTarefa = new Tarefa
+            {
+                Titulo = titulo,
+                Concluida = false
+            };
+
+            _tarefaRepository.Adicionar(novaTarefa);
+
+            return novaTarefa;
+        }
+
+        public List<Tarefa> ObterTodas()
+        {
+            return _tarefaRepository.ObterTodos();
+        }
+
+        public Tarefa ObterPorId(int id)
+        {
+            return _tarefaRepository.ObterPorId(id);
+        }
+
+        public void MarcarComoConcluida(int id, bool concluida)
+        {
+            Tarefa tarefaExistente = _tarefaRepository.ObterPorId(id);
+
+            if (tarefaExistente != null)
+            {
+                tarefaExistente.Concluida = concluida;
+                _tarefaRepository.Atualizar(tarefaExistente);
+            }
+        }
+
+        public bool RemoverTarefa(int id)
+        {
+            Tarefa tarefaExistente = _tarefaRepository.ObterPorId(id);
+
+            if (tarefaExistente != null)
+            {
+                _tarefaRepository.Remover(id);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
