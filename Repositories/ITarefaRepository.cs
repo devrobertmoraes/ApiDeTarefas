e51803490682201466c0d9a6cc43a22df15580e1
Robert Moraes <robertmoraes13r@gmail.com>
2025-07-09 22:44:03 -0300

    feat: Cria a interface ITarefaRepository

---
+++ Repositories/ITarefaRepository.cs
@@ -0,0 +1,13 @@
+﻿using ApiDeTarefas.Entities;
+
+namespace ApiDeTarefas.Repositories
+{
+    public interface ITarefaRepository
+    {
+        void Adicionar(Tarefa tarefa);
+        Tarefa ObterPorId(int id);
+        List<Tarefa> ObterTodos();
+        void Atualizar(Tarefa tarefa);
+        void Remover(int id);
+    }
+}
